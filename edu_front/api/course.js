3739bb50b7e6deeb4a8bbd771d5102e185e70216
Pokaboo <1152401192@qq.com>
2020-09-07 22:27:32 +0800

    【A】首页课程分类

---
+++ edu_front/api/course.js
@@ -0,0 +1,19 @@
+import request from "@/utils/request";
+
+export default {
+  //获取课程分页列表
+  getPageList(page, limit, searchObj) {
+    return request({
+      url: `/eduservice/frontcourse/pageCourseList/${page}/${limit}`,
+      method: "post",
+      data: searchObj
+    });
+  },
+  // 获取课程二级分类
+  findAllSubject() {
+    return request({
+      url: `/eduservice/subject/findAllSubject`,
+      method: "get"
+    });
+  }
+};
